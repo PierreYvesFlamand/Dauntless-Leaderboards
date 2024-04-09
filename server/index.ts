48ab6f0a9ef4fdf360d62470a23dda4290f16443
Pierre-Yves Flamand <pyflamand@gmail.com>
2024-04-09 13:09:54 +0200

    test

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ const port = 8888;
 app.use(cors());
 
 app.use('/', express.static(path.resolve(__dirname, './public/website/browser')));
-app.use('/data', express.static(path.resolve(__dirname, './public/data')));
+// app.use('/data', express.static(path.resolve(__dirname, './public/data')));
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
