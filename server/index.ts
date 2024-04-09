0b8cadc5755e0fc83fe826a963a935510746d838
Pierre-Yves Flamand <pyflamand@gmail.com>
2024-04-09 13:58:22 +0200

    test

--- server/index.ts
+++ server/index.ts
@@ -8,9 +8,9 @@ const port = 8888;
 app.use(cors());
 
 app.use(express.static(path.resolve(__dirname, './public/website/browser')));
-// app.use('/data', express.static(path.resolve(__dirname, './public/data')));
+app.use('/data', express.static(path.resolve(__dirname, './public/data')));
 
-app.get('/', (_req, res) => {
+app.get('*', (_req, res) => {
     res.sendFile(path.resolve(__dirname, './public/website/browser/index.html'));
 });
 
